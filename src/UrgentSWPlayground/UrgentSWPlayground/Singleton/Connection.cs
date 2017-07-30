576703c5a46e1cfe7050822268f5a3c21d703657
gabrielbressani <g.bressaniribeiro@gmail.com>
2017-07-30 16:55:19 -0300

    Start Repository

---
+++ src/UrgentSWPlayground/UrgentSWPlayground/Singleton/Connection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrgentSWPlayground.Singleton
+{
+    public sealed class Connection
+    {
+        private static VssConnection _connection;
+
+        private Connection() { }
+
+        public static VssConnection Open()
+        {
+
+            if (_connection == null)
+            {
+                var accountUri = new Uri("https://stratws.visualstudio.com");
+                var personalAccessToken = "hv3l46ruhkmhq4qnrpq7evjsaj6onfmdhurti6kp2vmxfdwxmwoq";
+
+                _connection = new VssConnection(accountUri, new VssBasicCredential(string.Empty, personalAccessToken));              
+            }
+
+            return _connection;
+       }
+    }
+}
---
+++ src/UrgentSw/UrgentSw/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrgentSw.MvcApplication" Language="C#" %>
